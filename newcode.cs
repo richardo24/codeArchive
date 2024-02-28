1e7a9938625ce2b023c07883cdca399be253908d
Richard Retana <161596799+richardo24@users.noreply.github.com>
2024-02-28 14:11:35 +0200

    Create newcode.cs

---
+++ newcode.cs
@@ -0,0 +1,28 @@
+bool IsFileSource(LogEvent e)
+{
+    var source = e.GetSourceContext();
+    return source != null && (source.Equals("File", StringComparison.OrdinalIgnoreCase) || source.StartsWith("File/", StringComparison.OrdinalIgnoreCase));
+}
+
+bool IsApiQueryWarning(LogEvent e)
+{
+    if (e.Level <= LogEventLevel.Warning
+        && e.GetPropertyValue<string>("RequestPath").EmptyNull().StartsWithNoCase("/odata/")
+        && e.GetSourceContext().EqualsNoCase("Microsoft.EntityFrameworkCore.Query"))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+ if (_seoSettings.CanonicalUrlsEnabled)
+            {
+                model.CanonicalUrl = Url.RouteUrl("Category", new { model.SeName }, Request.Scheme);
+            }
+
+            if (query.IsSubPage && !_catalogSettings.ShowDescriptionInSubPages)
+            {
+                model.Description.ChangeValue(string.Empty);
+                model.BottomDescription.ChangeValue(string.Empty);
+            }
